ee081e18415e46dd76b8f9ae756de87a4cc19a2c
Willie Tong <tongsinyin@gmail.com>
2016-04-02 20:55:37 +0800

    implemented quiz update

--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -1,4 +1,7 @@
 class Quiz < ActiveRecord::Base
   has_many :questions
   belongs_to :user
+
+  # This is required so that we can update the nested Questions data in bulk
+  accepts_nested_attributes_for :questions
 end
